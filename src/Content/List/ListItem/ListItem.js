147e671f40cc8277f40dd2ea45f64e1f93ee6edc
Matthew McCord <mattm.eternal@gmail.com>
2018-08-17 17:31:10 -0500

    Create ListItem component

---
+++ src/Content/List/ListItem/ListItem.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+
+import EditableInput from '../../../Input/EditableInput';
+
+import './ListItem.css';
+
+export default class ListItem extends Component {
+  render() {
+    const item = this.props.item;
+
+    return (
+      <div className="list_item__wrapper">
+
+        <div className="list_item--count-wrapper">
+          <h1 className="list_item--count">{ item.completedCount }x</h1>
+        </div>
+
+        <div className="list_item--contents">
+          <EditableInput
+            text={item.title}
+            changeCallback={this.props.titleChange}
+            titleBlurHandler={this.props.titleBlurHandler}
+            titleChangeHandler={this.props.titleChangeHandler}
+            editing={item.editingTitle}
+            index={this.props.index}
+          />
+
+          <div className="list_item--buttons">
+            <button
+                onClick={this.props.editCallback}
+                className="list_item--button edit__item-button"
+                data-index={this.props.index}
+              >
+                edit
+              </button>
+
+              <button
+                onClick={this.props.deleteCallback}
+                className="list_item--button delete__item-button"
+                data-index={this.props.index}
+              >
+                X
+              </button>
+
+              <button
+                  onClick={this.props.iDidItCallback}
+                  className="list_item--button increment__item-button"
+                  data-index={this.props.index}
+                >
+                  +
+              </button>
+          </div>
+        </div>
+
+
+      </div>
+
+
+    //   <table>
+    //   <tbody>
+    //     <tr className="list__row" key={index}>
+    //       <td>
+    //         <button
+    //           onClick={this.props.iDidItCallback}
+    //           data-index={this.props.index}
+    //         >
+    //           Oops! I did it
+    //         </button>
+    //       </td>
+
+    //       <td>
+    //         { item.completedCount } times
+    //       </td>
+
+    //       <td>
+    //         <EditableInput
+    //           text={item.title}
+    //           changeCallback={this.props.titleChange}
+    //           blurCallback={this.props.blurCallback}
+    //           editing={item.editingTitle}
+    //           index={this.props.index}
+    //         />
+    //       </td>
+    //       <td>
+    //         <button
+    //           onClick={this.props.editCallback}
+    //           className="edit__item-button"
+    //           data-index={this.props.index}
+    //         >
+    //           edit
+    //         </button>
+    //       </td>
+    //       <td>
+    //         <button
+    //           onClick={this.props.deleteCallback}
+    //           className="delete__item-button"
+    //           data-index={this.props.index}
+    //         >
+    //           X
+    //         </button>
+    //       </td>
+    //     </tr>
+    //   </tbody>
+    // </table>
+    )
+  }
+}
